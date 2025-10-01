65705012310923eb56180572e254262b33c4da8d
MHoang1302 <hoangminh13022006@gmail.com>
2025-10-01 11:14:27 +0700

    nhap

--- MyRespiratoryCheck.cpp
+++ MyRespiratoryCheck.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-    cout<<"Thang cho hung hung tinh"<<endl;
+    int a;
+    int b;
+    cout<<"Nhap a: ";
+    cin>>a;
+    cout<<"Nhap b: ";
+    cin>>b;
+    cout<<"Tong a + b = "<<a+b<<endl;
+    cout<<"Hieu a - b = "<<a-b<<endl;
+    cout<<"Tich a * b = "<<a*b<<endl;
+    if(b!=0)
+        cout<<"Thuong a / b = "<<a/b<<endl;
+    else
+        cout<<"Khong the chia cho 0"<<endl; 
     return 0;
 }
